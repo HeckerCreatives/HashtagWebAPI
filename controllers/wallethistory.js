d14395465707deaa1af238c2fd3cc98d55d01a2b
Joshupau <97903490+Joshupau@users.noreply.github.com>
2025-05-06 14:30:40 +0800

    poooosh

--- controllers/wallethistory.js
+++ controllers/wallethistory.js
@@ -240,4 +240,180 @@ exports.getwallettotalearnings = async (req, res) => {
     return res.json({message: "success", data: finaldata})
 }
 
+exports.getplayerwallethistoryforadmin = async (req, res) => {
+    const {id, username} = req.user
+    const {playerid, type, page, limit} = req.query
+
+    const pageOptions = {
+        page: parseInt(page) || 0,
+        limit: parseInt(limit) || 10
+    };
+    
+    let wallethistorypipeline
+
+    if (type == "creditwallet" || type == "minecoinwallet"){
+        wallethistorypipeline = [
+            {
+                $match: {
+                    owner: new mongoose.Types.ObjectId(playerid), 
+                    type: type
+                }
+            },
+            {
+                $lookup: {
+                    from: "staffusers",
+                    localField: "from",
+                    foreignField: "_id",
+                    as: "staffuserinfo"
+                }
+            },
+            {
+                $unwind: "$staffuserinfo"
+            },
+            {
+                $lookup: {
+                    from: "users",
+                    localField: "owner",
+                    foreignField: "_id",
+                    as: "userinfo"
+                }
+            },
+            {
+                $unwind: "$userinfo"
+            },
+            {
+                $project: {
+                    type: 1,
+                    amount: 1,
+                    fromusername: "$staffuserinfo.username",
+                    username: "$userinfo.username",
+                    createdAt: 1
+                }
+            },
+            {
+                $skip: pageOptions.page * pageOptions.limit
+            },
+            {
+                $limit: pageOptions.limit
+            }
+        ]
+    }
+    else{
+        wallethistorypipeline = [
+            {
+                $match: {
+                    owner: new mongoose.Types.ObjectId(playerid), 
+                    type: type
+                }
+            },
+            {
+                $lookup: {
+                    from: "users",
+                    localField: "from",
+                    foreignField: "_id",
+                    as: "fromuserinfo"
+                }
+            },
+            {
+                $unwind: "$fromuserinfo"
+            },
+            {
+                $lookup: {
+                    from: "users",
+                    localField: "owner",
+                    foreignField: "_id",
+                    as: "userinfo"
+                }
+            },
+            {
+                $unwind: "$userinfo"
+            },
+            {
+                $project: {
+                    type: 1,
+                    amount: 1,
+                    fromusername: "$fromuserinfo.username",
+                    username: "$userinfo.username",
+                    createdAt: 1
+                }
+            },
+            {
+                $skip: pageOptions.page * pageOptions.limit
+            },
+            {
+                $limit: pageOptions.limit
+            }
+        ]
+    }
+
+    const history = await Wallethistory.aggregate(wallethistorypipeline)
+    .catch(err => {
+
+        console.log(`Failed to get wallet history data for ${username}, wallet type: ${type}, player: ${playerid} error: ${err}`)
+
+        return res.status(401).json({ message: 'failed', data: `There's a problem with your account. Please contact customer support for more details` })
+    })
+    
+    const historypages = await Wallethistory.countDocuments({owner: new mongoose.Types.ObjectId(playerid), type: type})
+    .then(data => data)
+    .catch(err => {
+
+        console.log(`Failed to get wallet history count document data for ${username}, wallet type: ${type}, player: ${playerid} error: ${err}`)
+
+        return res.status(401).json({ message: 'failed', data: `There's a problem with your account. Please contact customer support for more details` })
+    })
+
+    const totalPages = Math.ceil(historypages / pageOptions.limit)
+
+    const data = {
+        history: [],
+        pages: totalPages
+    }
+
+    history.forEach(historydata => {
+        const {username, type, amount, fromusername, createdAt, _id} = historydata
+
+        data.history.push({
+            id: _id,
+            username: username,
+            type: type,
+            amount: amount,
+            fromusername: fromusername,
+            createdAt: createdAt
+        })
+    })
+
+    return res.json({message: "success", data: data})
+}
+exports.deleteplayerwallethistoryforadmin = async (req, res) => {
+    const { id, username } = req.user;
+    const { historyid } = req.body;
+
+    if (!historyid) {
+        return res.status(400).json({ message: "failed", data: "Incomplete form data." });
+    }
+
+    try {
+        // Fetch the wallet history entry
+        const history = await Wallethistory.findOne({ _id: new mongoose.Types.ObjectId(historyid) });
+        if (!history) {
+            return res.status(400).json({ message: "failed", data: "Wallet history not found." });
+        }
+
+
+        // delete the wallet history entry
+
+        await Wallethistory.findOneAndDelete({ _id: new mongoose.Types.ObjectId(historyid) })
+        .then(data => data)
+        .catch(err => {
+            console.log(`There's a problem encountered while deleting ${historyid} wallet history. Error: ${err}`)
+            return res.status(400).json({ message: "bad-request", data: "There's a problem with the server. Please contact customer support for more details."})
+        })
+
+        return res.status(200).json({ message: "success" });
+    } catch (err) {
+        console.log(`Failed to delete wallet history for ${username}, history id: ${historyid}, Error: ${err}`);
+        return res.status(500).json({ message: "failed", data: "An error occurred while deleting the wallet history." });
+    }
+};
 //  #endregion
--- routes/wallethistory.js
+++ routes/wallethistory.js
@@ -1,9 +1,11 @@
 const router = require("express").Router()
-const { playerwallethistory, getwallettotalearnings } = require("../controllers/wallethistory")
+const { playerwallethistory, getwallettotalearnings, getplayerwallethistoryforadmin, deleteplayerwallethistoryforadmin } = require("../controllers/wallethistory")
 const { protectplayer, protectsuperadmin } = require("../middleware/middleware")
 
 router
     .get("/userwallethistory", protectplayer, playerwallethistory)
     .get("/getwallettotalearnings", protectplayer, getwallettotalearnings)
+    .get("/getplayerwallethistoryforadmin", protectsuperadmin, getplayerwallethistoryforadmin)
+    .post("/deleteplayerwallethistoryforadmin", protectsuperadmin, deleteplayerwallethistoryforadmin)
 
 module.exports = router;
