37215757de72c8a92f8d4b5f9ce5b0b6b6e0c190
Darel Honrejas <133852063+D4rzDev@users.noreply.github.com>
2025-05-07 16:08:59 +0800

    pooosh

--- controllers/wallethistory.js
+++ controllers/wallethistory.js
@@ -156,6 +156,175 @@ exports.playerwallethistory = async (req, res) => {
     return res.json({message: "success", data: data})
 }
 
+exports.getwalletstatistics = async (req, res) => {
+    const {id, username} = req.user
+
+    const finaldata = {
+        hashbot: 0,
+        referral: 0,
+        unilevel: 0
+    }
+
+    const statisticGame = await Wallethistory.aggregate([
+        { 
+            $match: { 
+                owner: new mongoose.Types.ObjectId(id), 
+                type: "minecoinwallet" 
+            } 
+        },
+        { 
+            $group: { 
+                _id: null, 
+                totalAmount: { $sum: "$amount" } 
+            } 
+        }
+    ])
+    .catch(err => {
+        console.log(`There's a problem getting the statistics of earning game for ${username}. Error ${err}`)
+
+        return res.status(400).json({message: "bad-request", data : "There's a problem getting the statistics of earning game. Please contact customer support."})
+    })
+
+    if (statisticGame.length > 0) {
+        finaldata.game = statisticGame[0].totalAmount;
+    }
+
+    const statisticReferral = await Wallethistory.aggregate([
+        { 
+            $match: { 
+                owner: new mongoose.Types.ObjectId(id), 
+                type: "directcommissionwallet" 
+            } 
+        },
+        { 
+            $group: { 
+                _id: null, 
+                totalAmount: { $sum: "$amount" } 
+            } 
+        }
+    ])
+    .catch(err => {
+        console.log(`There's a problem getting the statistics of Referral for ${username}. Error ${err}`)
+
+        return res.status(400).json({message: "bad-request", data : "There's a problem getting the statistics of Referral. Please contact customer support."})
+    })
+
+    if (statisticReferral.length > 0) {
+        finaldata.referral = statisticReferral[0].totalAmount;
+    }
+
+    const statisticUnilevel = await Wallethistory.aggregate([
+        { 
+            $match: { 
+                owner: new mongoose.Types.ObjectId(id), 
+                type: "commissionwallet" 
+            } 
+        },
+        { 
+            $group: { 
+                _id: null, 
+                totalAmount: { $sum: "$amount" } 
+            } 
+        }
+    ])
+    .catch(err => {
+        console.log(`There's a problem getting the statistics of Unilevel ${username}. Error ${err}`)
+
+        return res.status(400).json({message: "bad-request", data : "There's a problem getting the statistics of Unilevel. Please contact customer support."})
+    })
+
+    if (statisticUnilevel.length > 0) {
+        finaldata.unilevel = statisticUnilevel[0].totalAmount;
+    }
+
+    return res.json({message: "success", data: finaldata})
+}
+
+
+exports.getwalletstatisticssuperadmin = async (req, res) => {
+    const {id} = req.query
+
+    const finaldata = {
+        hashbot: 0,
+        referral: 0,
+        unilevel: 0
+    }
+
+    const statisticGame = await Wallethistory.aggregate([
+        { 
+            $match: { 
+                owner: new mongoose.Types.ObjectId(id), 
+                type: "minecoinwallet" 
+            } 
+        },
+        { 
+            $group: { 
+                _id: null, 
+                totalAmount: { $sum: "$amount" } 
+            } 
+        }
+    ])
+    .catch(err => {
+        console.log(`There's a problem getting the statistics of earning game for ${username}. Error ${err}`)
+
+        return res.status(400).json({message: "bad-request", data : "There's a problem getting the statistics of earning game. Please contact customer support."})
+    })
+
+    if (statisticGame.length > 0) {
+        finaldata.game = statisticGame[0].totalAmount;
+    }
+
+    const statisticReferral = await Wallethistory.aggregate([
+        { 
+            $match: { 
+                owner: new mongoose.Types.ObjectId(id), 
+                type: "directcommissionwallet" 
+            } 
+        },
+        { 
+            $group: { 
+                _id: null, 
+                totalAmount: { $sum: "$amount" } 
+            } 
+        }
+    ])
+    .catch(err => {
+        console.log(`There's a problem getting the statistics of Referral for ${username}. Error ${err}`)
+
+        return res.status(400).json({message: "bad-request", data : "There's a problem getting the statistics of Referral. Please contact customer support."})
+    })
+
+    if (statisticReferral.length > 0) {
+        finaldata.referral = statisticReferral[0].totalAmount;
+    }
+
+    const statisticUnilevel = await Wallethistory.aggregate([
+        { 
+            $match: { 
+                owner: new mongoose.Types.ObjectId(id), 
+                type: "commissionwallet" 
+            } 
+        },
+        { 
+            $group: { 
+                _id: null, 
+                totalAmount: { $sum: "$amount" } 
+            } 
+        }
+    ])
+    .catch(err => {
+        console.log(`There's a problem getting the statistics of Unilevel ${username}. Error ${err}`)
+
+        return res.status(400).json({message: "bad-request", data : "There's a problem getting the statistics of Unilevel. Please contact customer support."})
+    })
+
+    if (statisticUnilevel.length > 0) {
+        finaldata.unilevel = statisticUnilevel[0].totalAmount;
+    }
+
+    return res.json({message: "success", data: finaldata})
+}
+
 exports.getwallettotalearnings = async (req, res) => {
     const {id, username} = req.user
 
--- routes/wallethistory.js
+++ routes/wallethistory.js
@@ -1,8 +1,10 @@
 const router = require("express").Router()
-const { playerwallethistory, getwallettotalearnings, getplayerwallethistoryforadmin, deleteplayerwallethistoryforadmin } = require("../controllers/wallethistory")
+const { playerwallethistory, getwallettotalearnings, getplayerwallethistoryforadmin, deleteplayerwallethistoryforadmin, getwalletstatistics, getwalletstatisticssuperadmin } = require("../controllers/wallethistory")
 const { protectplayer, protectsuperadmin } = require("../middleware/middleware")
 
 router
+    .get("/getwalletstatistics", protectplayer, getwalletstatistics)
+    .get("/getwalletstatisticssuperadmin", protectsuperadmin, getwalletstatisticssuperadmin)
     .get("/userwallethistory", protectplayer, playerwallethistory)
     .get("/getwallettotalearnings", protectplayer, getwallettotalearnings)
     .get("/getplayerwallethistoryforadmin", protectsuperadmin, getplayerwallethistoryforadmin)
